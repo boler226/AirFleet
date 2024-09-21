7a060c3841aef9f220962231924eb5c956e13ab3
Illya <illamatviuk@gmail.com>
2024-09-21 16:15:02 +0300

    Upgrade Seeder check if exist file.

--- src/models/SeederData.cpp
+++ src/models/SeederData.cpp
@@ -8,27 +8,40 @@ SeederData::SeederData() {
 }
 
 std::future<std::string> SeederData::SeedData() {
-    return std::async(std::launch::async, [this]() -> std::string {
+        return std::async(std::launch::async, [this]() -> std::string {
+            std::filesystem::path filePath = std::filesystem::current_path() / "..\\info";
 
-        TicketModel ticket1("John", "Doe", {1, 2, 3}, 3,
-                             (Destination &) Destination::Albania, (Destination &) Destination::Andorra);
-        PlaneModel plane1("AAN 120", 120);
+            if(!std::filesystem::exists(filePath)) {
+                std::filesystem::create_directory(filePath);
+            }
+
+            if(std::filesystem::is_empty(filePath))
+            {
+                TicketModel ticket1("John", "Doe", {1, 2, 3}, 3,
+                                    (Destination &) Destination::Albania, (Destination &) Destination::Andorra);
+                PlaneModel plane1("AAN 120", 120);
 
 
-        std::vector<FlightModel> testFlights = {
-                FlightModel(1, (Destination &) Destination::Albania, (Destination &) Destination::Andorra, {(Destination &) Destination::Belgium, (Destination &) Destination::Croatia}, 0, 0, 21, {ticket1}, plane1),
-        };
+                std::vector<FlightModel> testFlights = {
+                        FlightModel(1, (Destination &) Destination::Albania, (Destination &) Destination::Andorra, {(Destination &) Destination::Belgium, (Destination &) Destination::Croatia}, 0, 0, 21, {ticket1}, plane1),
+                };
 
-        std::vector<std::future<std::string>> results;
-        for (const auto& flight : testFlights) {
-            results.push_back(flightController.Create(flight));
-        }
+                std::vector<std::future<std::string>> results;
+                for (const auto& flight : testFlights) {
+                    results.push_back(flightController.Create(flight));
+                }
 
-        // Завершення всіх задач
-        for (auto& result : results) {
-            std::cout << result.get() << std::endl;
-        }
+                // Завершення всіх задач
+                for (auto& result : results) {
+                    std::cout << result.get() << std::endl;
+                }
 
-        return "Seeding completed.";
-    });
+                return "Seeding completed.";
+            }
+            else
+            {
+                std::cerr << "Seeding not completed. Maybe data already exist." << std::endl;
+            }
+
+        });
 }
