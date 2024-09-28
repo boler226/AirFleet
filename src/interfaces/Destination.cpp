3bcf28c1a0a0c1c4f539a2f58087a391af3b2ea8
Illya <illamatviuk@gmail.com>
2024-09-28 19:48:14 +0300

    Changed data about country to 'map' in Destination. Added toString, fromString and new constructor.

--- src/interfaces/Destination.cpp
+++ src/interfaces/Destination.cpp
@@ -1,63 +1,81 @@
 #include <cstring>
+#include <utility>
 #include "Destination.h"
 
 Destination::Destination()
         : info(Destination::Empty)
 {
 }
+Destination::Destination(CountryInfo  countryInfo)
+        : info(std::move(countryInfo))
+{
+}
+
+Destination Destination::fromString(const std::string &str) {
+    if (!str.empty()) {
+        auto item = countries.find(str);
+        if (item != countries.end()) {
+            return (Destination &)item->second;
+        }
+    }
+    return Destination(Destination::Empty);
+}
 
 std::string Destination::toString() const {
-
     return info.name;
 }
 
-const CountryInfo Destination::Empty = { "-", {} };
-const CountryInfo Destination::Albania = { "Albania", {"Tirana", "Durres", "Vlore", "Shkoder"} };
-const CountryInfo Destination::Andorra = { "Andorra", {"Andorra la Vella", "Escaldes-Engordany", "Encamp", "La Massana"} };
-const CountryInfo Destination::Armenia = { "Armenia", {"Yerevan", "Gyumri", "Vanadzor", "Hrazdan"} };
-const CountryInfo Destination::Austria = { "Austria", {"Vienna", "Graz", "Linz", "Salzburg"} };
-const CountryInfo Destination::Azerbaijan = { "Azerbaijan", {"Baku", "Ganja", "Sumqayit", "Mingachevir"} };
-const CountryInfo Destination::Belarus = { "Belarus", {"Minsk", "Gomel", "Mogilev", "Vitebsk"} };
-const CountryInfo Destination::Belgium = { "Belgium", {"Brussels", "Antwerp", "Ghent", "Charleroi"} };
-const CountryInfo Destination::BosniaAndHerzegovina = { "Bosnia and Herzegovina", {"Sarajevo", "Banja Luka", "Tuzla", "Zenica"} };
-const CountryInfo Destination::Bulgaria = { "Bulgaria", {"Sofia", "Plovdiv", "Varna", "Burgas"} };
-const CountryInfo Destination::Croatia = { "Croatia", {"Zagreb", "Split", "Rijeka", "Osijek"} };
-const CountryInfo Destination::Cyprus = { "Cyprus", {"Nicosia", "Limassol", "Larnaca", "Famagusta"} };
-const CountryInfo Destination::CzechRepublic = { "Czech Republic", {"Prague", "Brno", "Ostrava", "Plzen"} };
-const CountryInfo Destination::Denmark = { "Denmark", {"Copenhagen", "Aarhus", "Odense", "Aalborg"} };
-const CountryInfo Destination::Estonia = { "Estonia", {"Tallinn", "Tartu", "Narva", "Parnu"} };
-const CountryInfo Destination::Finland = { "Finland", {"Helsinki", "Espoo", "Tampere", "Vantaa"} };
-const CountryInfo Destination::France = { "France", {"Paris", "Marseille", "Lyon", "Toulouse"} };
-const CountryInfo Destination::Georgia = { "Georgia", {"Tbilisi", "Kutaisi", "Batumi", "Rustavi"} };
-const CountryInfo Destination::Germany = { "Germany", {"Berlin", "Hamburg", "Munich", "Cologne"} };
-const CountryInfo Destination::Greece = { "Greece", {"Athens", "Thessaloniki", "Patras", "Heraklion"} };
-const CountryInfo Destination::Hungary = { "Hungary", {"Budapest", "Debrecen", "Szeged", "Miskolc"} };
-const CountryInfo Destination::Iceland = { "Iceland", {"Reykjavik", "Kopavogur", "Hafnarfjordur", "Akureyri"} };
-const CountryInfo Destination::Ireland = { "Ireland", {"Dublin", "Cork", "Limerick", "Galway"} };
-const CountryInfo Destination::Italy = { "Italy", {"Rome", "Milan", "Naples", "Turin"} };
-const CountryInfo Destination::Kazakhstan = { "Kazakhstan", {"Nur-Sultan", "Almaty", "Shymkent", "Karaganda"} };
-const CountryInfo Destination::Kosovo = { "Kosovo", {"Pristina", "Mitrovica", "Peja", "Gjakova"} };
-const CountryInfo Destination::Latvia = { "Latvia", {"Riga", "Daugavpils", "Liepaja", "Jelgava"} };
-const CountryInfo Destination::Liechtenstein = { "Liechtenstein", {"Vaduz", "Schaan", "Balzers", "Triesen"} };
-const CountryInfo Destination::Lithuania = { "Lithuania", {"Vilnius", "Kaunas", "Klaipeda", "Siauliai"} };
-const CountryInfo Destination::Luxembourg = { "Luxembourg", {"Luxembourg City", "Esch-sur-Alzette", "Differdange", "Dudelange"} };
-const CountryInfo Destination::Malta = { "Malta", {"Valletta", "Birkirkara", "Mosta", "Qormi"} };
-const CountryInfo Destination::Moldova = { "Moldova", {"Chisinau", "Balti", "Tiraspol", "Bender"} };
-const CountryInfo Destination::Monaco = { "Monaco", {"Monaco", "Monte Carlo", "La Condamine", "Fontvieille"} };
-const CountryInfo Destination::Montenegro = { "Montenegro", {"Podgorica", "Niksic", "Herceg Novi", "Pljevlja"} };
-const CountryInfo Destination::Netherlands = { "Netherlands", {"Amsterdam", "Rotterdam", "The Hague", "Utrecht"} };
-const CountryInfo Destination::NorthMacedonia = { "North Macedonia", {"Skopje", "Bitola", "Kumanovo", "Prilep"} };
-const CountryInfo Destination::Norway = { "Norway", {"Oslo", "Bergen", "Stavanger", "Trondheim"} };
-const CountryInfo Destination::Poland = { "Poland", {"Warsaw", "Krakow", "Lodz", "Wroclaw"} };
-const CountryInfo Destination::Portugal = { "Portugal", {"Lisbon", "Porto", "Braga", "Coimbra"} };
-const CountryInfo Destination::Romania = { "Romania", {"Bucharest", "Cluj-Napoca", "Timisoara", "Iasi"} };
-const CountryInfo Destination::SanMarino = { "San Marino", {"San Marino", "Serravalle", "Borgo Maggiore", "Domagnano"} };
-const CountryInfo Destination::Serbia = { "Serbia", {"Belgrade", "Novi Sad", "Nis", "Kragujevac"} };
-const CountryInfo Destination::Slovakia = { "Slovakia", {"Bratislava", "Kosice", "Presov", "Zilina"} };
-const CountryInfo Destination::Slovenia = { "Slovenia", {"Ljubljana", "Maribor", "Celje", "Kranj"} };
-const CountryInfo Destination::Spain = { "Spain", {"Madrid", "Barcelona", "Valencia", "Seville"} };
-const CountryInfo Destination::Sweden = { "Sweden", {"Stockholm", "Gothenburg", "Malmo"} };
-const CountryInfo Destination::Turkey = { "Turkey", {"Ankara", "Istanbul", "Izmir", "Antalya"} };
-const CountryInfo Destination::Ukraine = { "Ukraine", {"Kyiv", "Kharkiv", "Lviv", "Odesa"} };
-const CountryInfo Destination::UnitedKingdom = { "United Kingdom", {"London", "Manchester", "Birmingham", "Edinburgh"} };
-const CountryInfo Destination::VaticanCity = { "Vatican City", {"Vatican City"} };
+const CountryInfo Destination::Empty = {"", {}};
+
+
+const std::unordered_map<std::string, CountryInfo> Destination::countries = {
+        { "Albania", { "Albania", { "Tirana", "Durres", "Vlore", "Shkoder" } }},
+        { "Andorra", { "Andorra", { "Andorra la Vella", "Escaldes-Engordany", "Encamp", "La Massana" } }},
+        { "Armenia", { "Armenia", { "Yerevan", "Gyumri", "Vanadzor", "Hrazdan" } }},
+        { "Austria", { "Austria", { "Vienna", "Graz", "Linz", "Salzburg" } }},
+        { "Azerbaijan", { "Azerbaijan", { "Baku", "Ganja", "Sumqayit", "Mingachevir" } }},
+        { "Belarus", { "Belarus", { "Minsk", "Gomel", "Mogilev", "Vitebsk" } }},
+        { "Belgium", { "Belgium", { "Brussels", "Antwerp", "Ghent", "Charleroi" } }},
+        { "Bosnia and Herzegovina", { "Bosnia and Herzegovina", { "Sarajevo", "Banja Luka", "Tuzla", "Zenica" } }},
+        { "Bulgaria", { "Bulgaria", { "Sofia", "Plovdiv", "Varna", "Burgas" } }},
+        { "Croatia", { "Croatia", { "Zagreb", "Split", "Rijeka", "Osijek" } }},
+        { "Cyprus", { "Cyprus", { "Nicosia", "Limassol", "Larnaca", "Famagusta" } }},
+        { "Czech Republic", { "Czech Republic", { "Prague", "Brno", "Ostrava", "Plzen" } }},
+        { "Denmark", { "Denmark", { "Copenhagen", "Aarhus", "Odense", "Aalborg" } }},
+        { "Estonia", { "Estonia", { "Tallinn", "Tartu", "Narva", "Parnu" } }},
+        { "Finland", { "Finland", { "Helsinki", "Espoo", "Tampere", "Vantaa" } }},
+        { "France", { "France", { "Paris", "Marseille", "Lyon", "Toulouse" } }},
+        { "Georgia", { "Georgia", { "Tbilisi", "Kutaisi", "Batumi", "Rustavi" } }},
+        { "Germany", { "Germany", { "Berlin", "Hamburg", "Munich", "Cologne" } }},
+        { "Greece", { "Greece", { "Athens", "Thessaloniki", "Patras", "Heraklion" } }},
+        { "Hungary", { "Hungary", { "Budapest", "Debrecen", "Szeged", "Miskolc" } }},
+        { "Iceland", { "Iceland", { "Reykjavik", "Kopavogur", "Hafnarfjordur", "Akureyri" } }},
+        { "Ireland", { "Ireland", { "Dublin", "Cork", "Limerick", "Galway" } }},
+        { "Italy", { "Italy", { "Rome", "Milan", "Naples", "Turin" } }},
+        { "Kazakhstan", { "Kazakhstan", { "Astana", "Almaty", "Shymkent", "Karaganda" } }},
+        { "Latvia", { "Latvia", { "Riga", "Daugavpils", "Liepaja", "Jelgava" } }},
+        { "Lithuania", { "Lithuania", { "Vilnius", "Kaunas", "Klaipeda", "Siauliai" } }},
+        { "Luxembourg", { "Luxembourg", { "Luxembourg", "Esch-sur-Alzette", "Differdange", "Dudelange" } }},
+        { "Malta", { "Malta", { "Valletta", "Birkirkara", "Mosta", "Qormi" } }},
+        { "Moldova", { "Moldova", { "Chisinau", "Balti", "Tiraspol", "Bender" } }},
+        { "Monaco", { "Monaco", { "Monaco-Ville", "Monte Carlo", "La Condamine", "Fontvieille" } }},
+        { "Montenegro", { "Montenegro", { "Podgorica", "Niksic", "Herceg Novi", "Budva" } }},
+        { "Netherlands", { "Netherlands", { "Amsterdam", "Rotterdam", "The Hague", "Utrecht" } }},
+        { "North Macedonia", { "North Macedonia", { "Skopje", "Bitola", "Kumanovo", "Prilep" } }},
+        { "Norway", { "Norway", { "Oslo", "Bergen", "Stavanger", "Trondheim" } }},
+        { "Poland", { "Poland", { "Warsaw", "Krakow", "Lodz", "Wroclaw" } }},
+        { "Portugal", { "Portugal", { "Lisbon", "Porto", "Amadora", "Braga" } }},
+        { "Romania", { "Romania", { "Bucharest", "Cluj-Napoca", "Timisoara", "Iasi" } }},
+        { "Russia", { "Russia", { "Moscow", "Saint Petersburg", "Novosibirsk", "Yekaterinburg" } }},
+        { "San Marino", { "San Marino", { "San Marino", "Serravalle", "Borgo Maggiore", "Domagnano" } }},
+        { "Serbia", { "Serbia", { "Belgrade", "Novi Sad", "Niš", "Kragujevac" } }},
+        { "Slovakia", { "Slovakia", { "Bratislava", "Kosice", "Presov", "Zilina" } }},
+        { "Slovenia", { "Slovenia", { "Ljubljana", "Maribor", "Celje", "Kranj" } }},
+        { "Spain", { "Spain", { "Madrid", "Barcelona", "Valencia", "Seville" } }},
+        { "Sweden", { "Sweden", { "Stockholm", "Gothenburg", "Malmo", "Uppsala" } }},
+        { "Switzerland", { "Switzerland", { "Bern", "Zurich", "Geneva", "Basel" } }},
+        { "Turkey", { "Turkey", { "Ankara", "Istanbul", "Izmir", "Bursa" } }},
+        { "Ukraine", { "Ukraine", { "Kyiv", "Kharkiv", "Odessa", "Dnipro" } }},
+        { "United Kingdom", { "United Kingdom", { "London", "Birmingham", "Glasgow", "Liverpool" } }},
+        { "Vatican City", { "Vatican City", { "Vatican City" } }}
+};
