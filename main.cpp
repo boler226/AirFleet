d25adbbd481b54199be20c302937c04ab7f5bd43
Illya <illamatviuk@gmail.com>
2024-10-27 10:36:39 +0200

    Upgraded SeederData.

--- main.cpp
+++ main.cpp
@@ -1,11 +1,19 @@
 #include "src/models/SeederData.h"
 
 int main() {
-//    SeederData seeder;
+    SeederData seeder;
     FlightController controller;
 //
-//    seeder.SeedData().get();
-    controller.List().get();
+    try {
+        std::string result = seeder.SeedData().get();
+        std::cout << "Result of seeding: " << result << std::endl;
+    } catch (const std::exception& e) {
+        std::cerr << "Exception caught during get: " << e.what() << std::endl;
+    } catch (...) {
+        std::cerr << "Unknown exception caught during get" << std::endl;
+    }
+
+//    controller.List().get();
 
 //    std::cout << std::time(nullptr);
 //    std::cout <<FlightModel::timeToString(std::time(0));
